cbc3bb7afbb4be1ae9e35f3b79730738a50bc6dd
Vinicius Nau <viniciuscoelhonau975@gmail.com>
2023-10-17 14:28:05 -0300

    feat: Reset PWD

--- src/Pages/ResetPassword/ResetPassword.tsx
+++ src/Pages/ResetPassword/ResetPassword.tsx
@@ -3,75 +3,78 @@ import Input from "../../Components/Forms/Input";
 import styles from "./ResetPassword.module.css";
 import { useDispatch, useSelector } from "react-redux";
 import Button from "../../Components/Forms/Button";
+import { fetchResetPassword } from "../../Services/Slices/resetPassword";
 import { handleKeyPress } from "../../Components/Helper";
 import Snackbar from "../../Components/Snackbar/Snackbar";
 import Loading from "../../Components/Loading/Loading";
-import { fetchResetPassword } from "../../Services/Slices/resetPassword";
 
 const ResetPassword = () => {
-  const dispatch = useDispatch();
+    const dispatch = useDispatch();
 
-  const [form, setForm] = useState<any>({
-    email: "",
-  });
-  const { data, error, loading } = useSelector(
-    (state: any) => state.resetPassword
-  );
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement> | any) => {
-    const { name, value } = e.target;
-    setForm((prev: any) => ({
-      ...prev,
-      [name]: value,
-    }));
-  };
-
-  const handleSubmit = () => {
-    dispatch<any>(fetchResetPassword(form));
-  };
-
-  if (loading)
-    return (
-      <div className={styles.loading}>
-        <Loading size="5rem" type="spin" />
-      </div>
+    const [form, setForm] = useState<any>({
+        email: "",
+    });
+    const { data, error, loading } = useSelector(
+        (state: any) => state.resetPassword
     );
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement> | any) => {
+        const { name, value } = e.target;
+        setForm((prev: any) => ({
+            ...prev,
+            [name]: value,
+        }));
+    };
 
-  return (
-    <div className={styles.container}>
-      {error && <Snackbar type="resetError" />}
-      {data && <Snackbar type="resetSuccess" />}
-      <div
-        className={styles.form}
-        onKeyUp={(e) => handleKeyPress(e, handleSubmit, "Enter")}
-      >
-        <h2 className={styles.title}>Redefinir Senha</h2>
-        <div className={styles.formGroup}>
-          <label className={styles.label} htmlFor="password">
-            Email:
-          </label>
-          <Input
-            type="email"
-            className={styles.input}
-            name="email"
-            value={form.email}
-            onChange={handleChange}
-          />
+    const handleSubmit = () => {
+        dispatch<any>(fetchResetPassword(form));
+    };
+
+    if (loading)
+        return (
+            <div
+                style={{
+                    display: "flex",
+                    height: "50vw",
+                    alignItems: "center",
+                    justifyContent: "center",
+                }}
+            >
+                <Loading size="5rem" type="spin" />
+            </div>
+        );
+
+    return (
+        <div className={styles.container}>
+            {error && <Snackbar type="resetError" />}
+            {data && data.message && <Snackbar type="resetSuccess" />}
+            <div
+                className={styles.form}
+                onKeyUp={(e) => handleKeyPress(e, handleSubmit, "Enter")}
+            >
+                <h2 className={styles.title}>Redefinir Senha</h2>
+                <Input
+                    type="email"
+                    className={styles.input}
+                    placeholder="Seu endereço de e-mail"
+                    name="email"
+                    value={form.email}
+                    onChange={handleChange}
+                />
+                <Button
+                    className={styles.button}
+                    onClick={handleSubmit}
+                    disabled={
+                        !(
+                            form.email.includes("@defensoria.sc.gov.br") ||
+                            form.email.includes("@defensoria.sc.def.br")
+                        )
+                    }
+                >
+                    Enviar E-mail
+                </Button>
+            </div>
         </div>
-        <Button
-          className={styles.button}
-          onClick={handleSubmit}
-          disabled={
-            !(
-              form.email.includes("@defensoria.sc.gov.br") ||
-              form.email.includes("@defensoria.sc.def.br")
-            )
-          }
-        >
-          Enviar E-mail
-        </Button>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default ResetPassword;
