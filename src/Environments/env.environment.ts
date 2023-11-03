f4672900d2dfe393775c51158367045ad3f65b83
Vinicius Nau <viniciuscoelhonau975@gmail.com>
2023-11-03 14:07:55 -0300

    fix: Remove Dev Environment

--- src/Environments/env.environment.ts
+++ src/Environments/env.environment.ts
@@ -1,5 +1,4 @@
 import { localEnvironment } from "./local.environment";
-import { devEnvironment } from "./dev.environment";
 import {prodEnvironment} from "./prod.environment";
 
 export const environment = prodEnvironment.apiUrl;
