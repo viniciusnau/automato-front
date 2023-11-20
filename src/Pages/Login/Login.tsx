30310ef9030256e89cfbc35643e3de55ecf71c04
Guilherme Pereira <guilhermepereira@defensoria.sc.gov.br>
2023-11-20 12:37:27 -0300

    Fix: alignment of inputs in login page and division line to login with Google

--- src/Pages/Login/Login.tsx
+++ src/Pages/Login/Login.tsx
@@ -74,13 +74,15 @@ const Login = () => {
           Bem vindo(a)
         </h2>
         <div className={styles.formGroup}>
-          <Input
-            className={styles.input}
-            label="Usuário"
-            name="username"
-            onChange={handleChange}
-            value={form.username}
-          />
+          <div className={styles.password}>
+            <Input
+              className={styles.input}
+              label="Usuário"
+              name="username"
+              onChange={handleChange}
+              value={form.username}
+            />
+          </div>
         </div>
         <div className={styles.formGroup}>
           <div className={styles.password}>
@@ -101,6 +103,12 @@ const Login = () => {
             </div>
           </div>
         </div>
+        <div className={styles.lineContainer}>
+          <hr className={styles.line} />
+          <div className={styles.text}>ou</div>
+          <hr className={styles.line} />
+        </div>
+
         <form
           action="https://automato.defensoria.sc.def.br/api/google-redirect/"
           method="get"
