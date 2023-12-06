18b15a464fccdece018545a52173cc6f8d668370
Guilherme Pereira <guilhermepereira@defensoria.sc.gov.br>
2023-12-06 12:47:35 -0300

    Feat: import audioplayer and apply slider

--- src/Components/AudioPlayer/AudioPlayer.tsx
+++ src/Components/AudioPlayer/AudioPlayer.tsx
@@ -1,21 +1,60 @@
-import React from 'react';
-import AudioPlayer from 'react-h5-audio-player';
-import styles from './AudioPlayer.module.css'
-import 'react-h5-audio-player/lib/styles.css';
+// import React from "react";
+// import styles from "./AudioPlayer.module.css";
+// import AudioPlayer from "react-h5-audio-player";
+// import "react-h5-audio-player/lib/styles.css";
+// import { acceptedFormats } from "../Helper";
+
+// interface IPlayer {
+//   audioFile: any;
+// }
+
+// const Player: React.FC<IPlayer> = ({ audioFile }) => {
+
+//   const removeFilesWithInvalidFormat = (audioFile:string) => {
+//     const filteredFiles = files.filter((file) => {
+//       const fileExtension = file.name.split(".").pop();
+//       return acceptedFormats.includes(fileExtension.toLowerCase());
+//     });
+//     return filteredFiles;
+//   };
+
+//   return (
+//     <div>
+//       <AudioPlayer
+//         className={styles.container}
+//         src={URL.createObjectURL(audioFile)}
+//         header={removeFilesWithInvalidFormat(audioFile.name)}
+//       />
+//     </div>
+//   );
+// };
+
+// export default Player;
+
+import React from "react";
+import styles from "./AudioPlayer.module.css";
+import AudioPlayer from "react-h5-audio-player";
+import "react-h5-audio-player/lib/styles.css";
+import { acceptedFormats } from "../Helper";
 
 interface IPlayer {
-    audioFile: any;
+  audioFile: any;
 }
 
-const Player: React.FC<IPlayer> = ({audioFile}) => {
-    return (
-        <div> 
-        {audioFile && (
-            <AudioPlayer className={styles.container} src={URL.createObjectURL(audioFile)} header={audioFile.name}  
-            />
-        )}
-        </div>
-    )
-}
+const Player: React.FC<IPlayer> = ({ audioFile }) => {
+  const removeFileFormat = (fileName: string) => {
+    return fileName.split(".")[0];
+  };
+
+  return (
+    <div>
+      <AudioPlayer
+        className={styles.container}
+        src={URL.createObjectURL(audioFile)}
+        header={removeFileFormat(audioFile.name)}
+      />
+    </div>
+  );
+};
 
 export default Player;
--- src/Pages/Transcribe/Transcribe.tsx
+++ src/Pages/Transcribe/Transcribe.tsx
@@ -1,288 +1,3 @@
-// import React, { useEffect, useState, ChangeEvent } from "react";
-// import styles from "./Transcribe.module.css";
-// import { useDispatch, useSelector } from "react-redux";
-// import Table from "../../Components/Table/Table";
-// import Input from "../../Components/Forms/Input";
-// import { fetchTranscribe } from "../../Services/Slices/transcribeSlice";
-// import { MdUpload } from "react-icons/md";
-// import Button from "../../Components/Forms/Button";
-// import { fetchUpload } from "../../Services/Slices/uploadSlice";
-// import Snackbar from "../../Components/Snackbar/Snackbar";
-// import { Slider } from "@mui/material";
-
-// const Transcribe: React.FC = () => {
-//   const dispatch = useDispatch();
-//   const [showSnackbar, setShowSnackbar] = useState<boolean>(false);
-//   const [sliderValues, setSliderValues] = useState<number[]>([0, 100]);
-//   const { data, error, loading } = useSelector(
-//     (state: any) => state.transcribeSlice
-//   );
-//   const uploadFile = useSelector((state: any) => state.uploadSlice);
-//   const [isColorInverted, setIsColorInverted] = useState<{
-//     [key: string]: boolean;
-//   }>({});
-
-//   const colorInvertedState = useSelector(
-//     (state: any) => state.a11ySlice.colorInverted
-//   );
-//   const [page, setPage] = useState<number>(1);
-//   const [isDispatched, setIsDispatched] = useState<boolean>(false);
-//   const [isResponsive, setIsResponsive] = useState<boolean>(false);
-//   const [form, setForm] = useState<any>({ file: null });
-//   const [isInvalidfile, setIsInvalidfile] = useState<boolean>(false);
-//   const [updated, setUpdated] = useState<any>([]);
-
-//   const columns = [
-//     { title: "Nome", property: "name" },
-//     { title: "Código", property: "code" },
-//     { title: "Data", property: "created_at" },
-//   ];
-
-//   const formatDate = (dateString: string) => {
-//     const options = {
-//       day: "2-digit",
-//       month: "2-digit",
-//       year: "numeric",
-//     } as Intl.DateTimeFormatOptions;
-//     return new Date(dateString).toLocaleDateString("pt-BR", options);
-//   };
-
-//   const update = () => {
-//     setUpdated(
-//       data?.results?.map((item: any) => ({
-//         name: item.name,
-//         created_at: formatDate(item.created_at),
-//         code: item.code,
-//         id: item.id,
-//       }))
-//     );
-//     const uploadFile = useSelector((state: any) => state.uploadSlice);
-//     const [isColorInverted, setIsColorInverted] = useState<{
-//         [key: string]: boolean;
-//     }>({});
-
-//   const handleSliderChange = (event: Event, newValues: number | number[]) => {
-//     setSliderValues(newValues as number[]);
-//   };
-
-//   const handleResize = () => {
-//     setIsResponsive(window.innerWidth <= 750);
-//   };
-
-//     const columns = [
-//         { title: 'Nome', property: 'name' },
-//         { title: 'Código', property: 'code' },
-//         { title: 'Data', property: 'created_at' },
-//     ];
-
-//     e.target.value = "";
-//   };
-
-//   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-//     e.preventDefault();
-//     if (form.file && validateFileFormat(form.file.name)) {
-//       const formData = new FormData();
-//       formData.append("audio_file", form.file);
-//       dispatch<any>(fetchUpload(formData));
-//       setIsInvalidfile(false);
-//       setShowSnackbar(true);
-//     }
-//     setForm({ file: null });
-//   };
-
-//   const validateFileFormat = (fileName: string) => {
-//     const acceptedFormats = ["mp3", "mp4", "wav", "flac", "amr", "ogg"];
-//     const fileExtension = fileName.split(".").pop()?.toLowerCase();
-//     const isValid = fileExtension && acceptedFormats.includes(fileExtension);
-//     isValid ? setIsInvalidfile(false) : setIsInvalidfile(true);
-//     return isValid;
-//   };
-
-//   useEffect(() => {
-//     window.addEventListener("resize", handleResize);
-//     handleResize();
-
-//     return () => {
-//       window.removeEventListener("resize", handleResize);
-//     };
-
-//     const update = () => {
-//         setUpdated(
-//             data?.results?.map((item: any) => ({
-//                 name: item.name,
-//                 created_at: formatDate(item.created_at),
-//                 code: item.code,
-//                 id: item.id,
-//             }))
-//         );
-//     };
-
-//     const handleResize = () => {
-//         setIsResponsive(window.innerWidth <= 750);
-//     };
-
-//     const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
-//         const file = e.target.files && e.target.files[0];
-//         setShowSnackbar(true);
-//         if (file && validateFileFormat(file.name)) {
-//             setForm((prev: any) => ({
-//                 ...prev,
-//                 file: file,
-//             }));
-//         }
-
-//         e.target.value = '';
-//     };
-
-//     const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-//         e.preventDefault();
-//         if (form.file && validateFileFormat(form.file.name)) {
-//             const formData = new FormData();
-//             formData.append('audio_file', form.file);
-//             dispatch<any>(fetchUpload(formData));
-//             setIsInvalidfile(false);
-//             setShowSnackbar(true);
-//         }
-//         setForm({ file: null });
-//     };
-
-//     const validateFileFormat = (fileName: string) => {
-//         const acceptedFormats = ['mp3', 'mp4', 'wav', 'flac', 'amr', 'ogg'];
-//         const fileExtension = fileName.split('.').pop()?.toLowerCase();
-//         const isValid =
-//             fileExtension && acceptedFormats.includes(fileExtension);
-//         isValid ? setIsInvalidfile(false) : setIsInvalidfile(true);
-//         return isValid;
-//     };
-
-//     const handleMouseDown = (e: any) => {
-//         document.addEventListener('mousemove', handleMouseMove);
-//         document.addEventListener('mouseup', handleMouseUp);
-//     };
-
-//     const handleMouseMove = (e: any) => {
-//         const newWidth = e.clientX;
-
-//         setBarWidth(newWidth);
-//     };
-
-//     const handleMouseUp = () => {
-//         document.removeEventListener('mousemove', handleMouseMove);
-//         document.removeEventListener('mouseup', handleMouseUp);
-//     };
-
-//     useEffect(() => {
-//         window.addEventListener('resize', handleResize);
-//         handleResize();
-
-//         return () => {
-//             window.removeEventListener('resize', handleResize);
-//         };
-//     }, []);
-
-//     useEffect(() => {
-//         dispatch<any>(fetchTranscribe(page.toString()));
-//         setIsDispatched(true);
-//     }, [dispatch, page, uploadFile.data?.response]);
-
-//     useEffect(() => {
-//         update();
-//     }, [data]);
-
-//     useEffect(() => {
-//         setShowSnackbar(false);
-//     }, []);
-
-//     useEffect(() => {
-//         setIsColorInverted(colorInvertedState);
-//     }, [colorInvertedState]);
-//     console.log(`form? `, form);
-//     return (
-//         <div className={styles.container}>
-//             {showSnackbar && uploadFile.data.response && (
-//                 <Snackbar
-//                     setShowSnackbar={setShowSnackbar}
-//                     type="transcribeSuccess"
-//                 />
-//             )}
-//             {showSnackbar &&
-//                 uploadFile?.error?.status &&
-//                 uploadFile?.error?.status !== 429 && (
-//                     <Snackbar
-//                         setShowSnackbar={setShowSnackbar}
-//                         type="transcribeError"
-//                     />
-//                 )}
-//             {showSnackbar && uploadFile?.error?.status === 429 && (
-//                 <Snackbar
-//                     setShowSnackbar={setShowSnackbar}
-//                     type="transcribeExceededError"
-//                 />
-//             )}
-//             {showSnackbar && isInvalidfile && (
-//                 <Snackbar
-//                     setShowSnackbar={setShowSnackbar}
-//                     type="invalidFileError"
-//                 />
-//             )}
-//             <div className={styles.postContainer}>
-//                 <label className={styles.fakeInput} htmlFor="file">
-//                     <MdUpload size={isResponsive ? 18 : 24} />
-//                 </label>
-//                 <Input
-//                     className={styles.file}
-//                     name="file"
-//                     type="file"
-//                     id="file"
-//                     onChange={handleFileChange}
-//                 />
-
-//                 <Button
-//                     className={`${styles.button} ${styles.schedule}`}
-//                     onClick={handleSubmit}
-//                 >
-//                     Transcrever
-//                 </Button>
-//             </div>
-//             <div className={styles.containera}>
-//                 <div
-//                     className={styles.fileText}
-//                     style={{ color: isColorInverted ? '#fafafa' : 'initial' }}
-//                 >
-//                 </div>
-//                 {form.file && uploadFile && (
-//                 <Player audioFile={form.file}  />
-//                 )}
-//         <div className={styles.slider}>
-//           <Slider
-//             value={sliderValues}
-//             onChange={handleSliderChange}
-//             aria-labelledby="range-slider"
-//             valueLabelDisplay="auto"
-//             valueLabelFormat={(value, index) => `${value}%`}
-//             min={0}
-//             max={100}
-//           />
-//         </div>
-//             </div>
-
-//             <Table
-//                 title="Transcrições em andamento"
-//                 columns={columns}
-//                 data={updated}
-//                 setPage={setPage}
-//                 page={page}
-//                 total={data.count}
-//                 isEmpty={isDispatched && data?.results?.length === 0}
-//                 loading={loading || uploadFile.loading}
-//                 error={error}
-//             />
-//         </div>
-//     );
-// };
-
-// export default Transcribe;
-
 import React, { useEffect, useState, ChangeEvent } from "react";
 import styles from "./Transcribe.module.css";
 import { useDispatch, useSelector } from "react-redux";
@@ -295,6 +10,7 @@ import { Slider } from "@mui/material";
 import Button from "../../Components/Forms/Button";
 import { MdUpload } from "react-icons/md";
 import Player from "../../Components/AudioPlayer/AudioPlayer";
+import { acceptedFormats } from "../../Components/Helper";
 
 const Transcribe: React.FC = () => {
   const dispatch = useDispatch();
@@ -304,13 +20,10 @@ const Transcribe: React.FC = () => {
     (state: any) => state.transcribeSlice
   );
   const uploadFile = useSelector((state: any) => state.uploadSlice);
-  const [isColorInverted, setIsColorInverted] = useState<{
+  const [isColorInverted] = useState<{
     [key: string]: boolean;
   }>({});
 
-  const colorInvertedState = useSelector(
-    (state: any) => state.a11ySlice.colorInverted
-  );
   const [page, setPage] = useState<number>(1);
   const [isDispatched, setIsDispatched] = useState<boolean>(false);
   const [isResponsive, setIsResponsive] = useState<boolean>(false);
@@ -367,7 +80,6 @@ const Transcribe: React.FC = () => {
   };
 
   const validateFileFormat = (fileName: string) => {
-    const acceptedFormats = ["mp3", "mp4", "wav", "flac", "amr", "ogg"];
     const fileExtension = fileName.split(".").pop()?.toLowerCase();
     const isValid = fileExtension && acceptedFormats.includes(fileExtension);
     isValid ? setIsInvalidfile(false) : setIsInvalidfile(true);
@@ -406,8 +118,6 @@ const Transcribe: React.FC = () => {
     setShowSnackbar(false);
   }, []);
 
-  console.log(`form? `, form);
-
   return (
     <div className={styles.container}>
       {showSnackbar && uploadFile.data.response && (
@@ -428,7 +138,6 @@ const Transcribe: React.FC = () => {
         <Snackbar setShowSnackbar={setShowSnackbar} type="invalidFileError" />
       )}
       <div className={styles.postContainer}>
-        {/* Label and input for file upload */}
         <label className={styles.fakeInput} htmlFor="file">
           <MdUpload size={isResponsive ? 18 : 24} />
         </label>
@@ -440,7 +149,6 @@ const Transcribe: React.FC = () => {
           onChange={handleFileChange}
         />
 
-        {/* Button to trigger transcription */}
         <Button
           className={`${styles.button} ${styles.schedule}`}
           onClick={handleSubmit}
@@ -449,32 +157,30 @@ const Transcribe: React.FC = () => {
         </Button>
       </div>
       <div className={styles.containera}>
-        {/* Placeholder for fileText */}
         <div
           className={styles.fileText}
           style={{ color: isColorInverted ? "#fafafa" : "initial" }}
-        >
-          {/* Your fileText content goes here */}
-        </div>
+        ></div>
 
-        {/* Conditionally render Player if form.file and uploadFile exist */}
-        {form.file && uploadFile && <Player audioFile={form.file} />}
+        {form.file && uploadFile && (
+          <div className={styles.audioContainer}>
+            <Player audioFile={form.file} />
 
-        {/* Slider component */}
-        <div className={styles.slider}>
-          <Slider
-            value={sliderValues}
-            onChange={handleSliderChange}
-            aria-labelledby="range-slider"
-            valueLabelDisplay="auto"
-            valueLabelFormat={(value, index) => `${value}%`}
-            min={0}
-            max={100}
-          />
-        </div>
+            <div style={{ width: "25vw" }}>
+              <Slider
+                value={sliderValues}
+                onChange={handleSliderChange}
+                aria-labelledby="range-slider"
+                valueLabelDisplay="auto"
+                valueLabelFormat={(value) => `${value}%`}
+                min={0}
+                max={100}
+              />
+            </div>
+          </div>
+        )}
       </div>
 
-      {/* Table component to display transcriptions */}
       <Table
         title="Transcrições em andamento"
         columns={columns}
